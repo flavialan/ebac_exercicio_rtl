9db0a8cabd190b470328c295f1f0b4f71f7c949b
Flavia Lopes <lalmeida.flavia@gmail.com>
2024-04-17 13:56:07 -0400

    Finalizando exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,27 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: 'Comentario 1',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        fireEvent.change(screen.getByTestId('comentario'), {
+            target: {
+                value: 'Comentario 2',
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+
+        expect(screen.getAllByTestId('comentario-post')).toHaveLength(2);
     });
 });
